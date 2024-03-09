b02bf2f32a24f4b99dd621981d02cba71af06cca
HDanDev <dhan_nayphotmail.com>
2024-03-09 17:34:35 +0100

    Additionnal verification checks

--- optimized.py
+++ optimized.py
@@ -9,27 +9,86 @@ VALID_SHARES = [share for share in DATASET if share.price > 0]
 # PRICES = [share.price for share in DATASET]
 # PROFITS = [share.profit for share in DATASET]
 
-# names = [
-#     "Share-ECAQ",
-#     "Share-IXCI",
-#     "Share-FWBE",
-#     "Share-ZOFA",
-#     "Share-PLLK",
-#     "Share-YFVZ",
-#     "Share-ANFX",
-#     "Share-PATS",
-#     "Share-NDKR",
-#     "Share-ALIY",
-#     "Share-JWGF",
-#     "Share-JGTW",
-#     "Share-FAPS",
-#     "Share-VCAX",
-#     "Share-LFXB",
-#     "Share-DWSK",
-#     "Share-XQII",
-#     "Share-ROOM"
-# ]
-# selected_objects = [obj for obj in VALID_SHARES if obj.name in names]
+names_sienna = [
+    "Share-ECAQ",
+    "Share-IXCI",
+    "Share-FWBE",
+    "Share-ZOFA",
+    "Share-PLLK",
+    "Share-YFVZ",
+    "Share-ANFX",
+    "Share-PATS",
+    "Share-NDKR",
+    "Share-ALIY",
+    "Share-JWGF",
+    "Share-JGTW",
+    "Share-FAPS",
+    "Share-VCAX",
+    "Share-LFXB",
+    "Share-DWSK",
+    "Share-XQII",
+    "Share-ROOM"
+]
+selected_sienna = [obj for obj in VALID_SHARES if obj.name in names_sienna]
+
+names_dp = [
+"Share-GYES",
+"Share-ZGFP",
+"Share-FWBE",
+"Share-PLLK",
+"Share-JWDZ",
+"Share-DEPW",
+"Share-MEQV",
+"Share-TQMM",
+"Share-LXZU",
+"Share-KRRA",
+"Share-RWIW",
+"Share-JCWZ",
+"Share-LAIC",
+"Share-OEYT",
+"Share-KOVS",
+"Share-ZKOZ",
+"Share-SCWM",
+"Share-DQXJ",
+"Share-ZLMC",
+"Share-JMLZ",
+"Share-FUGM",
+"Share-KPBW",
+"Share-VVYP",
+"Share-FCHD",
+"Share-PILL",
+"Share-MZLD",
+"Share-FAKH",
+"Share-TMRA",
+"Share-EOEN",
+"Share-LKSD",
+"Share-GIXZ",
+"Share-CXYC",
+"Share-DHIE",
+"Share-OWMP",
+"Share-BBNF",
+"Share-BPPA",
+"Share-GRVG",
+"Share-OCKK",
+"Share-BIJV",
+"Share-DSOO",
+"Share-IWTG",
+"Share-TGPO",
+"Share-VQQX",
+"Share-RBCS",
+"Share-LFXB",
+"Share-XYMR",
+"Share-YIFQ",
+"Share-DYVD",
+"Share-BMHD",
+"Share-XQII",
+"Share-FFZA",
+"Share-ROOM",
+"Share-FUDY",
+"Share-GEBJ"
+]
+selected_dp = [obj for obj in VALID_SHARES if obj.name in names_dp]
+
 
 def knapsack(actions, budget):
     n = len(actions)
@@ -56,8 +115,10 @@ selected_actions, total_profit = knapsack(VALID_SHARES, BUDGET_LIMIT)
 print("Selected actions using knapsack algorithm:")
 for action in selected_actions:
     print(action)
-print("Total price:", sum(action.price for action in selected_actions) / 100, "$")
-print("Total profit:", total_profit / 100, "%")
+print("Total price (Algorithm outcome):", sum(action.price for action in selected_actions) / 100, "$")
+print("Total profit (Algorithm outcome):", total_profit / 100, "%")
 print("Total profit (calculated):", sum(action.profit for action in selected_actions) / 100, "%")
-# print("Total price:", sum(action.price for action in selected_objects) / 100, "$")
-# print("Total profit (calculated):", sum(action.profit for action in selected_objects) / 100, "%")
+print("Sienna selected shares total price (from CSV):", sum(action.price for action in selected_sienna) / 100, "$")
+print("Sienna selected shares total profit (from CSV):", sum(action.profit for action in selected_sienna) / 100, "%")
+print("DP selected shares total price (from CSV):", sum(action.price for action in selected_dp) / 100, "$")
+print("DP selected shares total profit (from CSV):", sum(action.profit for action in selected_dp) / 100, "%")
